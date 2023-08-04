133625af57db8bd6b255139f3525faf8a6cc1ff4
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-08-03 22:34:45 -0600

    Usando codeigniter, layout e implementando gulp y sass

---
+++ app/Views/home.php
@@ -0,0 +1,10 @@
+<?php $this->extend("plantilla/layout"); ?>
+
+<?php $this->section("titulo");?>
+Prácticando CodeIgniter 4!
+<?php $this->endSection("titulo");?> 
+
+
+<?php $this->section("contenido");?>
+<h1>Hola mundo</h1>
+<?php $this->endSection("contenido"); ?>
